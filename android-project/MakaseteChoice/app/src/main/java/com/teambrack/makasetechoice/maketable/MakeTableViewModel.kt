8bef0cfae2cc93d79c4f628d50c3ce380f0f397e
宮西洋輔 <miyanishi1212@gmail.com>
2019-04-29 12:14:34 +0900

    feat: MakeTableのViewModelを追加

---
+++ android-project/MakaseteChoice/app/src/main/java/com/teambrack/makasetechoice/maketable/MakeTableViewModel.kt
@@ -0,0 +1,11 @@
+package com.teambrack.makasetechoice.maketable
+
+import androidx.lifecycle.ViewModel
+import com.teambrack.makasetechoice.NavigationController
+import javax.inject.Inject
+
+class MakeTableViewModel @Inject constructor(
+    private val navigator: NavigationController
+) : ViewModel() {
+
+}
