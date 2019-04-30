f341c8d062fcca5147a2ab1b8d901b3aa02a3119
宮西洋輔 <miyanishi1212@gmail.com>
2019-05-01 02:28:57 +0900

    feat: RecyclerViewの実装

--- android-project/MakaseteChoice/app/src/main/java/com/teambrack/makasetechoice/makegroup/MakeGroupViewModel.kt
+++ android-project/MakaseteChoice/app/src/main/java/com/teambrack/makasetechoice/makegroup/MakeGroupViewModel.kt
@@ -1,9 +1,12 @@
 package com.teambrack.makasetechoice.makegroup
 
 import androidx.databinding.ObservableArrayList
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.teambrack.makasetechoice.data.entity.GroupingEntity
 import com.teambrack.makasetechoice.data.entity.MemberEntity
 
 class MakeGroupViewModel : ViewModel() {
     val members = ObservableArrayList<MemberEntity>()
+    val grouping = MutableLiveData<List<GroupingEntity>>()
 }
