144676c15edd2cc43c1a1ecda48795e3e6a1d4e6
宮西洋輔 <miyanishi1212@gmail.com>
2019-05-02 00:41:41 +0900

    feat: シャッフルアルゴリズムの実装

--- android-project/MakaseteChoice/app/src/main/java/com/teambrack/makasetechoice/makegroup/MakeGroupViewModel.kt
+++ android-project/MakaseteChoice/app/src/main/java/com/teambrack/makasetechoice/makegroup/MakeGroupViewModel.kt
@@ -5,8 +5,31 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.teambrack.makasetechoice.data.entity.GroupingEntity
 import com.teambrack.makasetechoice.data.entity.MemberEntity
+import com.teambrack.makasetechoice.data.repository.MemberRepository
+import javax.inject.Inject
 
-class MakeGroupViewModel : ViewModel() {
+class MakeGroupViewModel @Inject constructor(
+    private val repository: MemberRepository
+) : ViewModel() {
     val members = ObservableArrayList<MemberEntity>()
     val grouping = MutableLiveData<List<GroupingEntity>>()
+    val groupingNumber = 1
+
+    fun onClickShuffleButton() {
+        grouping.value = shuffleMembers(groupingNumber, members)
+    }
+
+    private fun shuffleMembers(groupingNumber: Int, members: List<MemberEntity>): List<GroupingEntity> {
+        return members.shuffled().let { shuffleMembers ->
+            shuffleMembers.mapIndexed { index, memberEntity ->
+                index to memberEntity
+            }.groupBy({ (index, _) ->
+                index % groupingNumber
+            }, { (_, memberEntity) ->
+                memberEntity
+            }).flatMap {
+                listOf(GroupingEntity(it.key.toString(), it.value))
+            }
+        }
+    }
 }
