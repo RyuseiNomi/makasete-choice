642940134a8e0a764b22b135607270c42d152b1c
宮西洋輔 <miyanishi1212@gmail.com>
2019-06-29 14:50:04 +0900

    refactor: fix lint

--- vue-project/src/components/input_form.vue
+++ vue-project/src/components/input_form.vue
@@ -2,14 +2,18 @@
   <div class="people_name">
       <h2>{{ msg }}</h2>
       <p>編成の対象者を入力してください</p>
-      <input v-model="message" placeholder="名前を入力"><br>
-      <input v-model="message" placeholder="名前を入力"><br>
-      <input v-model="message" placeholder="名前を入力"><br>
-      <input v-model="message" placeholder="名前を入力"><br>
-      <input v-model="message" placeholder="名前を入力"><br>
-      <input v-model="message" placeholder="名前を入力"><br>
+      <label>
+          <input v-model="message" placeholder="名前を入力"><br>
+          <input v-model="message" placeholder="名前を入力"><br>
+          <input v-model="message" placeholder="名前を入力"><br>
+          <input v-model="message" placeholder="名前を入力"><br>
+          <input v-model="message" placeholder="名前を入力"><br>
+          <input v-model="message" placeholder="名前を入力"><br>
+      </label>
       <p>最大人数を入力してください</p>
-      <input v-model="message" placeholder="人数を入力">
+      <label>
+          <input v-model="message" placeholder="人数を入力">
+      </label>
   </div>
 </template>
 
