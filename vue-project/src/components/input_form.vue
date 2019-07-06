1e56e575fb116790199b9977b19ac5eb0243b467
宮西洋輔 <miyanishi1212@gmail.com>
2019-07-07 02:35:31 +0900

    feat: input_formの粒度を変更
    - 親コンポーネントがinput_formの数を決めるように変更
    - 子コンポーネント(input_form)は渡されたmemberを入力ボックスにバインドさせるだけ

--- vue-project/src/components/input_form.vue
+++ vue-project/src/components/input_form.vue
@@ -1,22 +1,19 @@
 <template>
   <div class="people_name">
       <label>
-          <input v-model="message" placeholder="名前を入力"><br>
-          <input v-model="message" placeholder="名前を入力"><br>
-          <input v-model="message" placeholder="名前を入力"><br>
-          <input v-model="message" placeholder="名前を入力"><br>
-          <input v-model="message" placeholder="名前を入力"><br>
-          <input v-model="message" placeholder="名前を入力"><br>
-      </label>
-      <p>最大人数を入力してください</p>
-      <label>
-          <input v-model="message" placeholder="人数を入力">
+          <input placeholder="名前を入力" v-model="member.name">
       </label>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'InputForm'
+  name: 'InputForm',
+    props: {
+        member: {
+            type: Object,
+            required: true
+        }
+    }
 }
 </script>
