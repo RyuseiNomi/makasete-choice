f42b86cda20326345105baeef3f4a3d8f3877f34
宮西洋輔 <miyanishi1212@gmail.com>
2019-07-07 02:37:04 +0900

    feat: 組み分け数を入力するためのコンポーネントを作成

---
+++ vue-project/src/components/InputSplitNum.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="split_num">
+        <p>組み分け数を入力してください</p>
+        <label>
+            <input type="number"
+                   placeholder="組み分け数を入力"
+                   v-model.number="splitNum"
+            />
+        </label>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "InputSplitNum",
+        data() {
+            return {
+                splitNum: 1
+            }
+        }
+
+    }
+</script>
