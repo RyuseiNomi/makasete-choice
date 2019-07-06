157e0fe477ed3d88924dd39acdb90e2953a051a2
宮西洋輔 <miyanishi1212@gmail.com>
2019-07-07 02:42:25 +0900

    feat: 配列をシャッフルする、shuffleメソッドを実装
    - [Fisher–Yatesアルゴリズム](https://qiita.com/komaji504/items/62a0f8ea43053e90555a#fisheryates%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0%E3%82%92%E7%94%A8%E3%81%84%E3%82%8B%E6%96%B9%E6%B3%95)を参考にした

--- vue-project/src/components/MakaseteChoice.vue
+++ vue-project/src/components/MakaseteChoice.vue
@@ -48,4 +48,15 @@
             }
         }
     }
+
+    function shuffle(targets) {
+        let array = targets;
+        for (let i = array.length - 1; i > 0; i--) {
+            const r = Math.floor(Math.random() * (i + 1));
+            const tmp = array[i];
+            array[i] = array[r];
+            array[r] = tmp;
+        }
+        return array
+    }
 </script>
