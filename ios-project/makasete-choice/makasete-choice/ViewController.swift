8d78166aa0d157e32d389a64dcc0295af13ac7bf
能美龍星 <nomiryuusei@nomiryuuseinoMacBook-puro.local>
2019-07-21 21:42:34 +0900

    iOSプロジェクトの新規作成

---
+++ ios-project/makasete-choice/makasete-choice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  makasete-choice
+//
+//  Created by 能美龍星 on 2019/07/21.
+//  Copyright © 2019 能美龍星. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios-project/makasete-choice/makasete-choiceTests/makasete_choiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  makasete_choiceTests.swift
+//  makasete-choiceTests
+//
+//  Created by 能美龍星 on 2019/07/21.
+//  Copyright © 2019 能美龍星. All rights reserved.
+//
+
+import XCTest
+@testable import makasete_choice
+
+class makasete_choiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
