be86274bd7ec5278db6df96cdb34a02e37f29a6b
宮西洋輔 <miyanishi1212@gmail.com>
2019-09-16 00:29:12 +0900

    fix(android): RepositoryのInject時にContextを渡すように改良

--- android-project/MakaseteChoice/app/src/main/java/com/teambrack/makasetechoice/di/modules/RepositoryModule.kt
+++ android-project/MakaseteChoice/app/src/main/java/com/teambrack/makasetechoice/di/modules/RepositoryModule.kt
@@ -1,5 +1,6 @@
 package com.teambrack.makasetechoice.di.modules
 
+import com.teambrack.makasetechoice.MakaseteChoiceApplication
 import com.teambrack.makasetechoice.data.repository.MemberRepository
 import dagger.Module
 import dagger.Provides
@@ -10,5 +11,6 @@ object RepositoryModule {
     @JvmStatic
     @Singleton
     @Provides
-    fun provideMemberRepository(): MemberRepository = MemberRepository()
+    fun provideMemberRepository(app: MakaseteChoiceApplication): MemberRepository =
+        MemberRepository(app.applicationContext)
 }
