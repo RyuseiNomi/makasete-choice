2f0e08ca47910872062c65639b86816360bc2cf0
宮西洋輔 <miyanishi1212@gmail.com>
2019-09-16 00:28:44 +0900

    feat(android): 保存と読み込み時に、キャッシュにデータがないときはSharedPreferencesを使用するようにした。

--- android-project/MakaseteChoice/app/src/main/java/com/teambrack/makasetechoice/data/repository/MemberRepository.kt
+++ android-project/MakaseteChoice/app/src/main/java/com/teambrack/makasetechoice/data/repository/MemberRepository.kt
@@ -1,13 +1,65 @@
 package com.teambrack.makasetechoice.data.repository
 
+import android.content.Context
+import androidx.core.content.edit
+import com.squareup.moshi.Moshi
+import com.teambrack.makasetechoice.data.ApplicationJsonAdapterFactory
 import com.teambrack.makasetechoice.data.entity.MemberEntity
+import se.ansman.kotshi.JsonSerializable
 
-class MemberRepository {
+class MemberRepository(
+    private val context: Context
+) {
+    private val moshi = Moshi.Builder()
+        .add(ApplicationJsonAdapterFactory.INSTANCE)
+        .build()
     private var cacheMembers: List<MemberEntity> = listOf()
+
     fun saveMembers(members: List<MemberEntity>): Boolean {
-        cacheMembers = members
+        saveLocal(members).also {
+            cacheMembers = members
+        }
         return !cacheMembers.isNullOrEmpty()
     }
 
-    fun loadMembers(): List<MemberEntity> = cacheMembers
+    fun loadMembers(): List<MemberEntity> {
+        return if (cacheMembers.isNullOrEmpty()) {
+            loadLocal().also {
+                cacheMembers = it
+            }
+        } else {
+            cacheMembers
+        }
+    }
+
+    private fun saveLocal(members: List<MemberEntity>) {
+        context.getSharedPreferences(
+            PREFERENCES_NAME,
+            Context.MODE_PRIVATE
+        ).edit {
+            moshi.adapter(MemberSaveTemplate::class.java)
+                .toJson(MemberSaveTemplate(members)).also { json ->
+                    putString(KEY, json)
+                }
+        }
+    }
+
+    private fun loadLocal(): List<MemberEntity> {
+        return context.getSharedPreferences(
+            PREFERENCES_NAME,
+            Context.MODE_PRIVATE
+        ).getString(KEY, null)?.let { data ->
+            moshi.adapter(MemberSaveTemplate::class.java)
+                .fromJson(data)
+        }?.members ?: emptyList()
+    }
+
+    @JsonSerializable
+    data class MemberSaveTemplate(val members: List<MemberEntity>)
+
+    companion object {
+        private val PACKAGE_NAME = MemberRepository::class.java.`package`
+        private val PREFERENCES_NAME = "${PACKAGE_NAME}_members"
+        private const val KEY = "members"
+    }
 }
