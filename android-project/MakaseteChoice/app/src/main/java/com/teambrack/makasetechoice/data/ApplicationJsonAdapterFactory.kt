0ed770024509a053e5ece6083e0ee4fdb793b9d0
宮西洋輔 <miyanishi1212@gmail.com>
2019-09-16 00:03:00 +0900

    feat(android): kotshiのJsonAdapterを作成

---
+++ android-project/MakaseteChoice/app/src/main/java/com/teambrack/makasetechoice/data/ApplicationJsonAdapterFactory.kt
@@ -0,0 +1,11 @@
+package com.teambrack.makasetechoice.data
+
+import com.squareup.moshi.JsonAdapter
+import se.ansman.kotshi.KotshiJsonAdapterFactory
+
+@KotshiJsonAdapterFactory
+abstract class ApplicationJsonAdapterFactory : JsonAdapter.Factory {
+    companion object {
+        val INSTANCE: ApplicationJsonAdapterFactory = KotshiApplicationJsonAdapterFactory
+    }
+}
